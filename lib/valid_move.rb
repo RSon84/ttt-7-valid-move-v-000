1f0eec8049054df32088c129537a340bac29ffdd
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 23:25:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,8 +8,8 @@ end
 
 def position_taken?(board, index)
    if board[index] != ("X") && board[index] != ("O")
-     return true
-   elsif board[index] == ("X") || board[index] == ("O")
      return false
+   elsif board[index] == ("X") || board[index] == ("O")
+     return true
    end
 end
