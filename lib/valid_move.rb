9325f489dda7c9efdc2148b0cd0309cc9337a8c3
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 22:08:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,12 @@
-
+board(0) = 1
+board(1) = 2
+board(2) = 3
+board(3) = 4
+board(4) = 5
+board(5) = 6
+board(6) = 7
+board(7) = 8
+board(8) = 9
 
 def valid_move?(board, index)
   if position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] && position_taken?(board, index) != ("X") && position_taken?(board, index) != ("O")
