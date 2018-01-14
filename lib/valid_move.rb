930f4de44c45132c5ce6e64548da6bcea1790411
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 21:40:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@ user_input = gets.chomp - 1
 
 def valid_move?(position_taken, board)
   if position_taken? != [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+end
 
 def position_taken?(board, index)
    if board[index] != ("X") && board[index] != ("O")
