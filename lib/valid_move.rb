993c105540457070ee5e0e8094cf4b5c94b51f85
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 22:58:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) != [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] && position_taken?(board, index) == ("X") && position_taken?(board, index) == ("O")
+  if position_taken?(board, index) != [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] || position_taken?(board, index) || ("X") && position_taken?(board, index) == ("O")
     return false
   elsif position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] && position_taken?(board, index) != ("X") && position_taken?(board, index) != ("O")
     return true
