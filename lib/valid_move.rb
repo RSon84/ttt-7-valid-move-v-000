be680921ca2b5fdb6b0b7acc468a20481f643bf0
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 22:47:39 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) == number.between?(0, 8)  
+  if position_taken?(board, index) == number.between?(0, 8)
     return true
   else position_taken?(board, index) != ("X")
     return true
   else position_taken?(board, index) != ("Y")
     return true
-  else
+  elsif
     return false
   end
 end
