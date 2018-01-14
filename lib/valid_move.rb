2f8f27071077d6b7d15a6dd301ec1b254d57098d
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 23:10:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+  if position_taken?(board, index) == board[index].between?(0, 8)
     return true
-  else position_taken?(board, index) != [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+  else position_taken?(board, index) != board[index].between?(0, 8)
     return false
   end
 end
