8c8272766e17514114b050400cadedf8fdf5fd4c
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 21:47:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+  if position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] && position_taken?(board, index) != ("X") && position_taken?(board, index) != ("O")
     return true
   end
 end
