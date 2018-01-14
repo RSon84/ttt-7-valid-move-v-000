c6f896cf8be2c4fef9e167f03e61b865328cfd98
Devin Wilson <craigvin84@yahoo.com>
2018-01-14 22:47:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,16 @@
-
-
 def valid_move?(board, index)
-  if position_taken?(board, index) == [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8] && position_taken?(board, index) != ("X") && position_taken?(board, index) != ("O")
+  if position_taken?(board, index) == number.between?(0, 8)  
+    return true
+  else position_taken?(board, index) != ("X")
+    return true
+  else position_taken?(board, index) != ("Y")
     return true
   else
     return false
   end
 end
+
+
 
 def position_taken?(board, index)
    if board[index] != ("X") && board[index] != ("O")
